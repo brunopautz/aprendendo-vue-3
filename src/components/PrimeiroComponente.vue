3d9b39ea585499c49cdc81e5d28a16944164664d
Bruno Pautz de Oliveira <brunoppautz@gmail.com>
2022-02-22 19:39:39 -0300

    Multiplos eventos + dados enviado na funcao

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,13 +3,19 @@
         <h1>Ola vue!</h1>
         <p>Meu nome é {{ nome }}</p>
         <p>Funcao: {{ profissao }}</p>
-
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+
+import MultiplosEventos from './MultiplosEventos.vue'
+
 export default {
     name: 'Primeiro componente',
+    components: {
+        MultiplosEventos
+    },
     data() {
         return {
             nome: "Bruno",
