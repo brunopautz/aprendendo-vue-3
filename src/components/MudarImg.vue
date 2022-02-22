ca6cd3affa2d4f36a5d0b03c28614728585a46ea
Bruno Pautz de Oliveira <brunoppautz@gmail.com>
2022-02-22 20:09:54 -0300

    Emit

---
+++ src/components/MudarImg.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <button @click="$emit('mudarImagem')" >Mudar imagem perfil</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MudarImg',
+    emits: ['mudarImagem']
+}
+</script>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,16 +1,28 @@
 <template>
     <div>
         <img :src="avatar" :alt="descricao">
+        <MudarImg  @mudar-imagem="trocarImagem" />
     </div>
 </template>
 
 <script>
+
+import MudarImg from './MudarImg.vue'
+
 export default {
     name: 'Picture',
+    components: {
+        MudarImg
+    },
     data () {
         return {
             avatar: '/image/avatar.png',
             descricao: 'Bruno Pautz'
+        }
+    },
+    methods: {
+        trocarImagem(){
+            this.avatar = '/image/avatar2.png'
         }
     }
 }
