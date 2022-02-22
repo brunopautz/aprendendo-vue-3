0277542c12341dc38db27b1a93a43fd7fa808f57
Bruno Pautz de Oliveira <brunoppautz@gmail.com>
2022-02-22 19:46:04 -0300

    Reutilização de componentes

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,20 +1,30 @@
 <template>
     <div>
         <h1>Ola vue!</h1>
+        <Reutilizando />
+
         <p>Meu nome é {{ nome }}</p>
         <p>Funcao: {{ profissao }}</p>
         <MultiplosEventos />
+        <Reutilizando />
+        <Reutilizando />
+        <Reutilizando />
+        <Reutilizando />
+        <Reutilizando />
+
     </div>
 </template>
 
 <script>
 
 import MultiplosEventos from './MultiplosEventos.vue'
+import Reutilizando from './Reutilizando.vue'
 
 export default {
     name: 'Primeiro componente',
     components: {
-        MultiplosEventos
+        MultiplosEventos,
+        Reutilizando
     },
     data() {
         return {
